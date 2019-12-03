5f031e5d605c86b19492892c200fad3400d74825
Sareff <ilbezr@gmail.com>
2019-12-03 20:08:20 +0300

    Got sesskey!

--- hackkait20.py
+++ hackkait20.py
@@ -1,13 +1,37 @@
 from bs4 import BeautifulSoup
 from requests import *
+import json
+import re
+
+s = Session()
 
 URL = "http://dist.kait20.ru/login/index.php"
 p = {"index":0,"methodname":"core_fetch_notifications","args":{"contextid":1}}
 
-r = get(url = URL)
+r = s.get(url = URL)
 
 data = r.text
 soup = BeautifulSoup(data, 'html.parser')
 inputs = soup.find("input", {"name":"logintoken"})
-print(inputs.get('value'))
+token = inputs.get('value')
+
+data = {'anchor': '',
+        'logintoken': token,
+        'username': 'mpavlova',
+        'password': 'u1xr0Bh8E0'}
+
+r = s.post(url = URL, data = data)
+
+ans = BeautifulSoup(r.text, 'html.parser')
+print(ans)
+pattern = re.compile(r"M.cfg = (\{.*?});", re.MULTILINE | re.DOTALL)
+script = ans.find("script", text=pattern)
+obj = {}
+
+if script:
+    obj = pattern.search(script.text).group(1)
+    obj = json.loads(obj)
+
+print(obj["sesskey"])
+
 
