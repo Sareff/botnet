74739a0df0dee60bbcd5aa9344fca4213d41b952
Ilya Bezridniy <ilbezr@gmail.com>
2019-12-04 10:57:22 +0300

    added session

--- hackkait20.py
+++ hackkait20.py
@@ -1,13 +1,29 @@
 from bs4 import BeautifulSoup
 from requests import *
 
-URL = "http://dist.kait20.ru/login/index.php"
-p = {"index":0,"methodname":"core_fetch_notifications","args":{"contextid":1}}
+s = Session()
 
-r = get(url = URL)
+URL = "http://dist.kait20.ru/login/index.php"
+
+r = s.get(url = URL)
 
 data = r.text
 soup = BeautifulSoup(data, 'html.parser')
 inputs = soup.find("input", {"name":"logintoken"})
-print(inputs.get('value'))
+token = inputs.get('value')
+
+print(token)
+
+data = {'anchor': '',
+        'logintoken': token,
+        'username': 'yuandrejtseva',
+        'password': 'YWeTxTQS0P'}
+
+r = s.post(url = URL, data = data)
+answer = BeautifulSoup(r.text, 'html.parser')
+print(answer)
+course = answer.find_all("div", {"id": "page-content-container-2"})
+#gid = course.get("data-course-id")
+#print(gid)
+print(course)
 
